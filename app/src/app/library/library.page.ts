924a202326157abff5b6fce1020b0c172e1451de
CMathien <camille.mathien@gmail.com>
2023-03-09 18:33:10 +0000

    Structure library

--- app/src/app/library/library.page.ts
+++ app/src/app/library/library.page.ts
@@ -9,8 +9,29 @@ import { ApiService } from '../config/api.service';
 })
 export class LibraryPage {
 
+    pbooks: any;
+    user: any;
+
     constructor(public apiService: ApiService, public authenticationService: AuthenticationService) {
         if (!authenticationService.getAuthenticated()) window.location.assign("/tabs/account");
+        this.user = authenticationService.getLoggedUser();
     }
+
+    ngOnInit() {
+        this.loadLibrary();
+    }
+
+    loadLibrary() {
+        this.apiService.apiFetch(`/users/${this.user}/books`, "get", (res: any) => {
+            this.pbooks = res.data;
+        })
+    }
+
+    handleRefresh(event: any) {
+        setTimeout(() => {
+            this.loadLibrary();
+            event.target.complete();
+        }, 2000);
+    };
 
 }
